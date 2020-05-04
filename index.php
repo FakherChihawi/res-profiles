b0a0786aa9f72d4d3d1a4bb0e540e56aecb987ba
FakherChihawi <48892611+FakherChihawi@users.noreply.github.com>
2020-05-04 08:05:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,50 +1,123 @@
 <?php
-    //Connect DataBase
-    include_once 'pdo.php';
+	
+session_start();
 
-    $id = $_SESSION['user_id'];
-    $stmt = $pdo->prepare('SELECT * FROM profile WHERE user_id = $id ');
-    $rows = $stmt->fetch(PDO::FETCH_ASSOC);
+$logged_in = false;
+$profiles = [];
+
+if (isset($_SESSION['name']) ) 
+{
+
+	$logged_in = true;
+	$status = false;
+
+	if ( isset($_SESSION['status']) ) 
+	{
+		$status = htmlentities($_SESSION['status']);
+		$status_color = htmlentities($_SESSION['color']);
+
+		unset($_SESSION['status']);
+		unset($_SESSION['color']);
+	}
+
+	try 
+	{
+	    $pdo = new PDO("mysql:host=localhost;dbname=coursera_javascript_jquery_and_json", "root", "root");
+        // set the PDO error mode to exception
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	    $all_profiles = $pdo->query("SELECT * FROM profile");
+
+		while ( $row = $all_profiles->fetch(PDO::FETCH_OBJ) ) 
+		{
+		    $profiles[] = $row;
+		}
+	}
+	catch(PDOException $e)
+	{
+	    echo "Connection failed: " . $e->getMessage();
+	    die();
+	}
+}
+
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fakher Chihaoui</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container text-center">
-        <div class="jumbotron">
-        <h1 class="title">
-        Chuck Severance's Resume Registry
-        </h1>
-        <?php if( !isset($_SESSION['user_id'])): ?>
-        <a class="btn btn-secondary" href="login.php">Please log in</a>
-        <?php else: ?>
-            <a class="btn btn-secondary" href="logout.php">log out</a>
-            <?php if( isset($rows)): ?>
-            <table>
-                <th>
-                    <td>First Name</td>
-                    <td>Last Name</td>
-                    <td>Headline</td>
-                    <td>Sammry</td>
-                </th>
-                <?php foreach($rows as $row): ?>
-                    <td> <?php echo $row[2] ?> </td>
-                    <td> <?php echo $row[3] ?> </td>
-                    <td> <?php echo $row[5] ?> </td>
-                    <td> <?php echo $row[6] ?> </td>
-                <?php endforeach; ?>
-            </table>
-            <?php endif ?>
-        <?php endif; ?>   
-        <p>Note: Your implementation should retain data across multiple logout/login sessions. This sample implementation clears all its data periodically - which you should not do in your implementation.</p>
-    
-        </div>
-    </div>
-</body>
+<html>
+	<head>
+		<title>Ivan Neradovic's Resume Registry</title>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="container">
+			<h1>Ivan Neradovic's Resume Registry</h1>
+
+			<?php if (!$logged_in) : ?>
+				<p>
+					<a href="login.php">Please log in</a>
+				</p>
+				<p>
+					Attempt
+					<a href="add.php">add data</a> 
+					without logging in.
+				</p>
+			<?php else : ?>
+
+				<?php
+	                if ( $status !== false ) 
+	                {
+	                    // Look closely at the use of single and double quotes
+	                    echo(
+	                        '<p style="color: ' .$status_color. ';" class="col-sm-10">'.
+	                            $status.
+	                        "</p>\n"
+	                    );
+	                }
+	            ?>
+
+				<?php if (empty($profiles)) : ?>
+					<p>No rows found</p>
+				<?php else : ?>
+					<div class="row">
+						<div class="col-md-8">
+							<table class="table">
+								<thead>
+									<tr>
+										<th>Name</th>
+										<th>Headline</th>
+										<th>Action</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php foreach($profiles as $profile) : ?>
+				                        <tr>
+				                        	<td>
+				                        		<a href="view.php?profile_id=<?php echo $profile->profile_id; ?>">
+				                        			<?php echo $profile->first_name . ' ' . $profile->last_name; ?>
+				                        		</a>
+				                        	</td>
+											<td><?php echo $profile->headline ?></td>
+											<td>
+												<a href="edit.php?profile_id=<?php echo $profile->profile_id; ?>">
+													Edit
+												</a> / 
+												<a href="delete.php?profile_id=<?php echo $profile->profile_id; ?>">
+													Delete
+												</a>
+											</td>
+				                        </tr>
+				                    <?php endforeach; ?>
+								</tbody>
+							</table>
+						</div>
+					</div>
+				<?php endif; ?>
+				<p>
+					<a href="add.php">Add New Entry</a>
+				</p>
+				<p>
+					<a href="logout.php">Logout</a>
+				</p>
+			<?php endif; ?>	
+		</div>
+	</body>
 </html>
