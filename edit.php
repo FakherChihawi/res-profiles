e9704d3cdd2569d42b6b987f8f0f4ef9e1f15be9
FakherChihawi <48892611+FakherChihawi@users.noreply.github.com>
2020-05-04 08:22:08 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -105,7 +105,7 @@ if (isset($_REQUEST['profile_id']))
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Ivan Neradovic Autos</title>
+        <title>Fakher Chihawi Autos</title>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
