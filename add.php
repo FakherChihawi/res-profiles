239883d320ce65b7375af421908bd0157aad9517
FakherChihawi <48892611+FakherChihawi@users.noreply.github.com>
2020-05-04 08:17:50 +0200

    Update add.php

--- add.php
+++ add.php
@@ -87,7 +87,7 @@ if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['e
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Ivan Neradovic Autos</title>
+        <title>Fakher Chihawi Autos</title>
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     </head>
     <body>
